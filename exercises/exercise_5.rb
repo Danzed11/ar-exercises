9ce4ca81a4eb27945b4ebfda0eef3b7d9c4af903
Dan <dan.zajac@mac.com>
2019-06-12 00:54:29 +0000

    exercises complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+company_average = Store.average(:annual_revenue)
+million_co = Store.where("annual_revenue > ?", [1_000_000]).count
+
+puts total_revenue
+puts company_average
+puts million_co
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Mike", last_name: "Harharwood", hourly_rate: 87)
+@store2.employees.create(first_name: "Dan", last_name: "Zajac", hourly_rate: 90)
+@store2.employees.create(first_name: "Greg", last_name: "Leader", hourly_rate: 40)
